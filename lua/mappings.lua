c91657a421a53d253d7781a1d301c622fd3def4f
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-10-19 20:29:02 -0300

    Java code runner

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -27,4 +27,16 @@ map("n", "<leader><S-x>", function()
 	require("nvchad.tabufline").closeAllBufs(true)
 end, { desc = "Close all buffers" })
 
+map("n", "<A-r>", function()
+	local file = vim.fn.expand("%:p")
+	local classname = vim.fn.expand("%:t:r")
+
+	require("nvchad.term").runner({
+		pos = "vsp",
+		cmd = "javac " .. file .. " && java " .. classname,
+		id = "ekk",
+		clear_cmd = false,
+	})
+end, { desc = "Java code runner" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
