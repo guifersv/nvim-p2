855d9edca1e023cb34ad6dacc543fcba047e6fe2
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-10-20 13:16:53 -0300

    Change to nvim-jdtls

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,12 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "java", "jdtls" }
+local servers = { "jdtls" }
 vim.lsp.enable(servers)
 
-require("java")
-
-vim.lsp.config("jdtls", {
-	cmd = { vim.fn.expand("~/.local/share/nvim/mason/bin/jdtls") },
-	root_dir = vim.fs.root(0, { "gradlew", ".git", "mvnw" }),
-})
 -- read :h vim.lsp.config for changing options of lsp servers
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,6 +1,7 @@
 return {
 	{ "nvim-mini/mini.move", version = false },
 	{ "nvim-mini/mini.surround", version = false },
+  { "mfussenegger/nvim-jdtls" },
 
 	{
 		"stevearc/conform.nvim",
@@ -30,8 +31,6 @@ return {
 				"lua-language-server",
 				"stylua",
 				"jdtls",
-				"java-test",
-				"java-debug-adapter",
 			},
 		},
 	},
@@ -44,51 +43,6 @@ return {
 				"lua",
 				"vimdoc",
 				"java",
-			},
-		},
-	},
-	{
-		"nvim-java/nvim-java",
-		config = false,
-		dependencies = {
-			{
-				"neovim/nvim-lspconfig",
-				opts = {
-					servers = {
-						-- Your JDTLS configuration goes here
-						jdtls = {
-							-- settings = {
-							--   java = {
-							--     configuration = {
-							--       runtimes = {
-							--         {
-							--           name = "JavaSE-23",
-							--           path = "/usr/local/sdkman/candidates/java/23-tem",
-							--         },
-							--       },
-							--     },
-							--   },
-							-- },
-						},
-					},
-					setup = {
-						jdtls = function()
-							-- Your nvim-java configuration goes here
-							require("java").setup({
-								-- root_markers = {
-								--   "settings.gradle",
-								--   "settings.gradle.kts",
-								--   "pom.xml",
-								--   "build.gradle",
-								--   "mvnw",
-								--   "gradlew",
-								--   "build.gradle",
-								--   "build.gradle.kts",
-								-- },
-							})
-						end,
-					},
-				},
 			},
 		},
 	},
