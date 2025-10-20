ad97d2b65bc8233d3dad108e384b6603f1bdb70c
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-10-20 13:29:01 -0300

    Initial dap and neotest setup

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,7 +1,7 @@
 return {
 	{ "nvim-mini/mini.move", version = false },
 	{ "nvim-mini/mini.surround", version = false },
-  { "mfussenegger/nvim-jdtls" },
+	{ "mfussenegger/nvim-jdtls" },
 
 	{
 		"stevearc/conform.nvim",
@@ -45,5 +45,45 @@ return {
 				"java",
 			},
 		},
+	},
+	{
+		-- Debug Framework
+		"mfussenegger/nvim-dap",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+		},
+		-- config = function()
+		--   require "custom-config.nvim-dap"
+		-- end,
+		event = "VeryLazy",
+	},
+	{ "nvim-neotest/nvim-nio" },
+	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
+	{
+		"rcasia/neotest-java",
+		ft = "java",
+		dependencies = {
+			"mfussenegger/nvim-jdtls",
+			"mfussenegger/nvim-dap",
+			"rcarriga/nvim-dap-ui",
+		},
+	},
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		config = function()
+			require("neotest").setup({
+				adapters = {
+					require("neotest-java")({
+						-- config here
+					}),
+				},
+			})
+		end,
 	},
 }
