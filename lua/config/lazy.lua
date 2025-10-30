dd6d1ac66f0dddaa2aca3a3d8450e975e12928cf
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-10-30 13:00:58 -0300

    Initial dependencies

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -19,6 +19,9 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.coding.mini-surround" },
+    { import = "lazyvim.plugins.extras.editor.neo-tree" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "plugins" },
   },
   defaults = {
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "html",
+        "javascript",
+        "css",
+        "jdtls",
+        "json",
+        "lua",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
