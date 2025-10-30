e393186a4ec58370748322c4d7ed3e5ac5bc1ff7
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-10-30 17:32:20 -0300

    Add mappings

--- init.lua
+++ init.lua
@@ -1,2 +1,4 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
+vim.g.meutoken = "loveamor"
+vim.g.idquestao = "coracao"
+
 require("config.lazy")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,71 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<leader>jr", function()
+  local file = vim.fn.expand("%:p")
+  local classname = vim.fn.expand("%:t:r")
+  Snacks.terminal("javac " .. file .. " && java " .. classname, { cwd = LazyVim.root(), auto_close = false })
+end, { desc = "Java code runner" })
+
+map("n", "<leader>js", function()
+  local file = vim.fn.expand("%:t")
+  Snacks.terminal(
+    "python dirlididi.py submit " .. vim.g.idquestao .. " " .. vim.g.meutoken .. " " .. file,
+    { cwd = LazyVim.root(), auto_close = false }
+  )
+end, { desc = "Java Dirlididi code submit" })
+
+vim.keymap.set("n", "<Leader>jd", "<Plug>(doge-generate)", { desc = "Java Generate documatation" })
+
+vim.api.nvim_create_user_command("SetId", function(opts)
+  vim.g.idquestao = opts.args
+end, {
+  nargs = 1,
+})
+
+vim.api.nvim_create_user_command("SetToken", function(opts)
+  vim.g.meutoken = opts.args
+end, {
+  nargs = 1,
+})
+
+-- Debugger
+
+map("n", "<leader>dt", function()
+  require("dap").toggle_breakpoint()
+end, { desc = "Dap Toggle Breakpoint" })
+
+map("n", "<leader>dc", function()
+  require("dap").continue()
+end, { desc = "Dap Continue" })
+
+map("n", "<leader>di", function()
+  require("dap").step_into()
+end, { desc = "Dap Step Into" })
+
+map("n", "<leader>do", function()
+  require("dap").step_over()
+end, { desc = "Dap Step Over" })
+
+map("n", "<leader>ds", function()
+  require("dap").step_out()
+end, { desc = "Dap Step Out" })
+
+map("n", "<leader>dd", function()
+  require("dapui").toggle()
+end, { desc = "Dap UI Toggle" })
+
+map("n", "<leader>dl", function()
+  require("dap").run_last()
+end, { desc = "Dap Run Last" })
+
+map("n", "<leader>dr", function()
+  require("dap").repl.open()
+end, { desc = "Dap Open REPL" })
+
+map("n", "<leader>dq", function()
+  require("dap").terminate()
+  require("dapui").close()
+end, { desc = "Dap Terminate" })
