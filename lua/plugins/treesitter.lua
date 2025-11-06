530fd13c6894a4284d1dcff52c0b2765637815cb
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-11-06 11:39:30 +0000

    Remove redundant treesitter langs

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,14 +3,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
-        "html",
-        "javascript",
         "css",
-        "jdtls",
-        "json",
-        "lua",
-        "vim",
-        "yaml",
       },
     },
   },
