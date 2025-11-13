270fc7f33aeb837e6ce5cd256578b5a83fec7fe2
Guilherme Fernandes <gui.fer.sv@gmail.com>
2025-11-13 11:32:44 -0300

    Fix coc starting with blink

--- init.lua
+++ init.lua
@@ -4,3 +4,8 @@ vim.g.idquestao = "coracao"
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.conform")
+require("blink.cmp").setup({
+  enabled = function()
+    return not vim.b.blink_disable
+  end,
+})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,5 +18,6 @@ return {
   {
     "neoclide/coc.nvim",
     branch = "release",
+    ft = { "java" }, -- load only for Java filetypes
   },
 }
